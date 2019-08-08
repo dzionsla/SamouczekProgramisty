970decd1d1213e3739fef2c172b8e28fa803c891
dzionsla <dzionsla@gmail.com>
2019-08-08 14:38:11 +0200

    1st day part 2

--- src/main/java/com/dzionsla/aoc2016/day01/Movement.java
+++ src/main/java/com/dzionsla/aoc2016/day01/Movement.java
@@ -8,8 +8,10 @@ import java.util.stream.Stream;
 
 public class Movement {
 	Orientation orient = new Orientation();
-	String input1 = "R2, R2, R2";
-			
+	String input0 = "L2, R3";
+	String input1 = "R2, R2, R2, R2";
+	String input2 = "R5, L5, R5, R3";
+	String in = "R2, L5, L4, L5, R4, R1, L4, R5, R3, R1, L1, L1, R4, L4, L1, R4, L4, R4, L3, R5, R4, R1, R3, L1, L1, R1, L2, R5, L4, L3, R1, L2, L2, R192, L3, R5, R48, R5, L2, R76, R4, R2, R1, L1, L5, L1, R185, L5, L1, R5, L4, R1, R3, L4, L3, R1, L5, R4, L4, R4, R5, L3, L1, L2, L4, L3, L4, R2, R2, L3, L5, R2, R5, L1, R1, L3, L5, L3, R4, L4, R3, L1, R5, L3, R2, R4, R2, L1, R3, L1, L3, L5, R4, R5, R2, R2, L5, L3, L1, L1, L5, L2, L3, R3, R3, L3, L4, L5, R2, L1, R1, R3, R4, L2, R1, L1, R3, R3, L4, L2, R5, R5, L1, R4, L5, L5, R1, L5, R4, R2, L1, L4, R1, L1, L1, L5, R3, R4, L2, R1, R2, R1, R1, R3, L5, R1, R4";
 	String input = "R2, L5, L4, L5, R4, R1, L4, R5, R3, R1, L1, L1, R4, L4, L1, R4, L4, R4, L3, R5, "
 			+ "R4, R1, R3, L1, L1, R1, L2, R5, L4, L3, R1, L2, L2, R192, L3, R5, R48, R5, L2, R76, "
 			+ "R4, R2, R1, L1, L5, L1, R185, L5, L1, R5, L4, R1, R3, L4, L3, R1, L5, R4, L4, R4, "
@@ -26,7 +28,7 @@ public class Movement {
 		
 		list = parse(input1);
 		calculatePath();
-		orient.print();
+		Orientation.print();
 		
 	}
 	
@@ -47,14 +49,16 @@ public class Movement {
 		Pattern p = Pattern.compile("[A-Z]");
 		Matcher m = p.matcher(dir);
 		m.find();
+		//System.out.print(m.group(0));
 		return m.group(0);
 	}
 	
 	private int showValue(String dir) {
 		Pattern p = Pattern.compile("\\d+");
 		Matcher m = p.matcher(dir);
-		System.out.println(dir);
 		m.find();
+		//System.out.print(Integer.valueOf(m.group(0)) + " - ");
+		
 		return Integer.valueOf(m.group(0));
 	}
 	
--- src/main/java/com/dzionsla/aoc2016/day01/Orientation.java
+++ src/main/java/com/dzionsla/aoc2016/day01/Orientation.java
@@ -1,6 +1,46 @@
 package com.dzionsla.aoc2016.day01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Orientation{
+	
+	private class XY {
+		private int x;
+		private int y;
+		
+		public XY(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		@Override
+		public String toString() {
+			return "XY [x=" + x + ", y=" + y + "]";
+		}
+		
+		
+	}
+	
+	
+	public static List<XY> l = new ArrayList<XY>();
+
 	public enum Orient {
 		NORTH,
 		SOUTH,
@@ -42,40 +82,80 @@ public class Orientation{
 		this.x = 0;
 		this.y = 0;
 		this.orient = Orient.NORTH;
+		save(x, y);
+		showSaved();
 	}
 	
 	public void move(String turn, int howMany) {
 		
 		if (orient == Orient.NORTH) {
-			if (turn == "R") {
+			if (turn.equals("R")) {
 				x = x + howMany;
-			} else if(turn == "L") {
+				orient = Orient.EAST;
+			} else if(turn.equals("L")) {
 				x = x - howMany;
+				orient = Orient.WEST;
 			}
 		} else if(orient == Orient.EAST) {
-			if (turn == "R") {
-				y = y + howMany;
-			} else if(turn == "L") {
+			if (turn.equals("R")) {
 				y = y - howMany;
+				orient = Orient.SOUTH;
+			} else if(turn.equals("L")) {
+				y = y + howMany;
+				orient = Orient.NORTH;
 			}
 		} else if(orient == Orient.WEST) {
-			if (turn == "R") {
-				y = y - howMany;
-			} else if(turn == "L") {
+			if (turn.equals("R")) {
 				y = y + howMany;
+				orient = Orient.NORTH;
+			} else if(turn.equals("L")) {
+				y = y - howMany;
+				orient = Orient.SOUTH;
 			}
 		} else if(orient == Orient.SOUTH) {
-			if (turn == "R") {
+			if (turn.equals("R")) {
 				x = x - howMany;
-			} else if(turn == "L") {
+				orient = Orient.WEST;
+			} else if(turn.equals("L")) {
 				x = x + howMany;
+				orient = Orient.EAST;
 			}
 		}
 		
+		save(x, y);
+		showSaved();
+	}
+	
+	public void save(int x, int y) {
+
+		if (l.size() == 0) {
+			XY point = new XY(x, y);
+			l.add(point);
+		} else {
+			int localX = Math.abs(x - l.get(l.size() - 1).getX());
+			int localY = Math.abs(y - l.get(l.size() - 1).getY());
+			
+			if (localX != 0) {
+				for (int i = localX - 1; i >= 0; i--) {
+					l.add(new XY(x - i, y));
+				}
+			} else {
+				for (int i = localY - 1; i >= 0; i--) {
+					l.add(new XY(x, y - i));
+				}
+			}
+		}	
+		
+	}
+	
+	public void showSaved() {
+		System.out.println(l.get(l.size() - 1));
 	}
 	
 	public static void print() {
-		System.out.println("x = " + x + ", y = " + y);
+		System.out.println("Wspolrzedne: " + (x > 0 ? "Wschod": " Zachod") + " - " + x + (y > 0 ? "   Polnoc": " Poludnie") + " - " + y);
+		System.out.println("Od pozycji startowej blokow: " + (Math.abs(x)+Math.abs(y)));
+		//System.out.println(l.size());
 	}
 }
 
