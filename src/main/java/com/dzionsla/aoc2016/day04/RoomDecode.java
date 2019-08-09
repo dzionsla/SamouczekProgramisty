f941ed72ebfa2541395d95f62c462bee8b1d63cf
dzionsla <dzionsla@gmail.com>
2019-08-09 16:42:49 +0200

    4th

--- src/main/java/com/dzionsla/aoc2016/day04/RoomDecode.java
+++ src/main/java/com/dzionsla/aoc2016/day04/RoomDecode.java
@@ -5,16 +5,21 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class RoomDecode {
 	
-	String path = "C:\\projects\\java\\SamouczekProgramisty\\src\\main\\resources\\day04_input.txt";
+	String path = "C:\\projects\\java\\SamouczekProgramisty\\src\\main\\resources\\day04_input1.txt";
 	List<String> data = new ArrayList<String>();
 	List<String> hash = new ArrayList<String>();
+	Integer sum;
 	
 	public RoomDecode() throws IOException {
 		readData(path);
@@ -24,15 +29,23 @@ public class RoomDecode {
 	}
 	
 	public void loopData(List<String> data) {
+		int howMany = 0;
+		
 		for (String code : data) {
-			checkCode(code);
+			if (checkCode(code)) {
+				howMany++;
+			}
 		}
 	}
 	
 	public boolean checkCode(String code) {
 		System.out.print(code + ": ");
 		System.out.print(getIdent(code) + " - ");
-		System.out.println(getHash(code));
+		System.out.print(getHash(code) + "   :   ");
+		System.out.println(getLetters(code));
+		
+		System.out.println(countLetters(getLetters(code)));
+		
 		
 		return false;
 	}
@@ -53,12 +66,59 @@ public class RoomDecode {
 		return m1.group().substring(1, 6);
 	}
 	
-	public String getHassh(String line) {
+	public String getLetters(String line) {
+		String output = "";
+		Pattern p1 = Pattern.compile("[a-z]+-");
+		Matcher m1 = p1.matcher(line);
 		
+		while (m1.find()) {
+			output = output + m1.group();	
+		}
+		
+		output = output.replaceAll("-", "");
+		
+		return sortString(output);
+	}
+	
+	public String sortString(String str) {
+	    return str.chars()
+	      .sorted()
+	      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+	      .toString();
+	}
+	
+	public String countLetters(String str) {
+		HashMap<String, Integer> m = new HashMap<String, Integer>();
+		
+		for (char c : str.toCharArray()) {
+			if (m.containsKey(String.valueOf(c))) {
+				m.put(String.valueOf(c), m.get(String.valueOf(c)) + 1);
+			} else {
+				m.put(String.valueOf(c), 1);
+			}
+		}
+		
+		for (int i = 0; i < 6; i++) {
+			
+		}
+		m.entrySet().forEach(n -> System.out.print(n));
+		//m = sortMap(m);
+		//System.out.println(m);
+		
+		//System.out.println(sortMap(m));
 		
 		return "";
 	}
 	
+	private HashMap<String, Integer> sortMap(HashMap<String, Integer> map) {
+		return map.entrySet()
+				  .stream()
+				  .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				  .collect(Collectors.toMap(
+				    Map.Entry::getKey, 
+				    Map.Entry::getValue, 
+				    (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+	}
 	
 	public void readData(String path) throws IOException {
 		data = Files.lines(Paths.get(path), StandardCharsets.UTF_8).collect(Collectors.toList());
