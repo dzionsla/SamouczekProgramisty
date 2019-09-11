7f7d6d5674a6415f0c0659ea17e8f6c5b8f963ca
dzionsla <dzionsla@gmail.com>
2019-09-11 23:13:57 +0200

    20th finished

--- src/main/java/com/dzionsla/aoc2016/day20/IPRule.java
+++ src/main/java/com/dzionsla/aoc2016/day20/IPRule.java
@@ -6,6 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,80 +18,63 @@ import java.util.stream.Stream;
 
 public class IPRule {
 	private List<String> data;
-	private List<BigInteger> arr;
 	private String dir = "C:\\projects\\java\\SamouczekProgramisty\\src\\main\\resources\\day20_input.txt";
+	private List<IpRange> ranges = new ArrayList<IpRange>();;
 	
 	public IPRule() {
 		readData();
 		
-		//data.forEach(n -> addToArr(n));
+        for (String string : data) {
+        	ranges.add(IpRange.parse(string));
+		}
+        
+        Collections.sort(this.ranges);
+        
+        System.out.println(getFirstAllowedIp());
+        System.out.println(getAllowedIpsCount());
 
-		System.out.println(findFirst());
-		//System.out.println(long);
 	}
+
 	
-	
-	private long findFirst() {
-		long firstAllowedIp = 0;
-        for (String data : data) {
-            if (firstAllowedIp < getMax(data)) {
+    public long getFirstAllowedIp() {
+        long firstAllowedIp = 0;
+        for (IpRange range : ranges) {
+            if (firstAllowedIp < range.start) {
                 return firstAllowedIp;
             }
-            if (getMax(data) > firstAllowedIp) {
-                firstAllowedIp = getMin(data) + 1;
+            if (range.end > firstAllowedIp) {
+                firstAllowedIp = range.end + 1;
             }
         }
         return firstAllowedIp;
-	}
+    }
+    
+    public long getAllowedIpsCount() {
+        long allowedIP = 0;
+        long counts = 0;
+        for (IpRange range : ranges) {
+            if (allowedIP < range.start) {
+            	counts = counts + range.start - allowedIP;
+			} 
+            if (range.end > allowedIP) {
+            	allowedIP = range.end + 1;
+            }
+        }
+        return counts;
+    }
 	
-	private void addToArr(String str) {
-		Pattern p = Pattern.compile("(.+)-(.+)");
-		Matcher m = p.matcher(str);
-		
-		if (!m.matches()) {
-	    	throw new IllegalArgumentException(String.format("Instruction [%s] can't be parsed!", str));
-	    }
-		BigInteger start = new BigInteger(m.group(1));
-		BigInteger end = new BigInteger(m.group(2));
+//	private long getMin(String str) {
+//		Pattern p = Pattern.compile("(.+)-(.+)");
+//		Matcher m = p.matcher(str);
+//		
+//		if (!m.matches()) {
+//	    	throw new IllegalArgumentException(String.format("Instruction [%s] can't be parsed!", str));
+//	    }
+//		long end = Long.parseLong(m.group(1));
+//		
+//		return end;
+//	}
 
-	}
-	
-	private long getMax(String str) {
-		Pattern p = Pattern.compile("(.+)-(.+)");
-		Matcher m = p.matcher(str);
-		
-		if (!m.matches()) {
-	    	throw new IllegalArgumentException(String.format("Instruction [%s] can't be parsed!", str));
-	    }
-		long start = Long.parseLong(m.group(2));
-		
-		return start;
-	}
-	
-	private long getMin(String str) {
-		Pattern p = Pattern.compile("(.+)-(.+)");
-		Matcher m = p.matcher(str);
-		
-		if (!m.matches()) {
-	    	throw new IllegalArgumentException(String.format("Instruction [%s] can't be parsed!", str));
-	    }
-		long end = Long.parseLong(m.group(1));
-		
-		return end;
-	}
-	
-	private void generateTable(BigInteger from, BigInteger limit) {
-		BigInteger x;
-		x = new BigInteger("4294967295");
-
-		for (BigInteger i = BigInteger.valueOf(0); i.compareTo(x) < 0 ; i = i.add(BigInteger.ONE)) {
-			//System.out.println(i);
-			if (x.compareTo(i) > 0) {
-				System.out.println(i);
-			}
-		}
-	}
-	
 	private void readData() {
 		try {
 			data = Files.readAllLines(Paths.get(dir));
